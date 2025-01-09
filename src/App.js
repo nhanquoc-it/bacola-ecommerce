5c9ffd760a1c6e4e9a0a4ea524ffd114d5a305b4
nhanquoc-it <caphuunhanquoc2510@gmail.com>
2025-01-09 10:31:11 +0700

    config babel & path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Header from "./components/Header/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+
+// Import Components
+import Header from "~/components/Header/Header.jsx";
+import Home from "~/pages/Home/Home.jsx";
 
 function App() {
 	return (
